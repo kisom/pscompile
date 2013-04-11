2c05cce0d3b4b4501d97a5910241e599626f84cb
Kyle Isom <coder@kyleisom.net>
2013-04-11 12:49:39 -0600

    simplify `compile-ps` code

--- pscompile.lisp
+++ pscompile.lisp
@@ -19,15 +19,12 @@
 (defun compile-ps (filename)
   (let ((javascript (ps:ps-compile-file filename))
         (js-filename (ppcre:regex-replace "\\.[^.]+$" filename ".js")))
-    (when (or (probe-file (make-pathname :name js-filename))
-              (probe-file (make-pathname :directory '(:absolute "") :name js-filename)))
-      (delete-file js-filename))
-    (with-open-file (in filename
-                        :direction :input)
+    (with-open-file (out js-filename
+                         :direction :output
+                         :if-does-not-exist :create
+                         :if-exists :supersede)
       (with-standard-io-syntax
-        (do ((form (read in) (read in nil :eof)))
-            ((eql :eof form) nil)
-          (append-js-form form js-filename))))
+        (princ javascript out)))
     (format t "[+] ~A -> ~A complete~%" filename js-filename)))
 
 (defun compile-ps-file (filename)
@@ -38,12 +35,3 @@
 (defun report-error (e filename)
   (format t "~&    ~A~%" e)
   (format t "[!] error compiling ~A~%" filename))
-
-(defun append-js-form (form js-filename)
-  (with-open-file (out js-filename
-                      :direction :output
-                      :if-does-not-exist :create
-                      :if-exists :append)
-    (with-standard-io-syntax
-      (princ (ps:ps* form) out)
-      (format out "~%"))))
